85f23bb58a7e7c5b8bd9e4dda67b41cc44a2b8e5
PE <pierre-emmanuel.marrel@etu.univ-lyon1.fr>
2023-02-22 12:44:37 +0100

    voulit voulot

--- cmdDis.py
+++ cmdDis.py
@@ -38,11 +38,12 @@ def serveur_path(ctx):
     description = descriptionR,
     help = helpR
 )
-async def r(ctx, number: int = 100):
+async def r(ctx,number: int = 100):
     user = ctx.author.display_name
-    value = int (rand.rand(number))
-    rand.ajouterValeur(value, ctx.guild.name, user)
+    value = int(rand.rand(number))
     x = await ctx.send(f"```{ctx.author.display_name} "+ made + str(value) + space + on + "[" + str(number) + "]```")
+    value = (value / (number + 1 - rand.min ) * 100)
+    rand.ajouterValeur(value, ctx.guild.name, user)
     await ctx.message.delete()
     if value == rand.min:
         await x.add_reaction('\N{CROSS MARK}')
